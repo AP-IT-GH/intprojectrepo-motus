1570318dabaaa55a08eac65807512bf81a55c88f
Bart <s110266@ap.be>
2020-02-18 15:13:28 +0100

    Added Firebase

--- Android/app/src/main/java/com/example/motus/ShowActivity.java
+++ Android/app/src/main/java/com/example/motus/ShowActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.example.motus.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -19,6 +21,8 @@ public class ShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("data");
         Intent intent = getIntent();
         lijst = intent.getStringArrayListExtra("dataList");
         TextView box = findViewById(R.id.dataBox);
@@ -26,6 +30,7 @@ public class ShowActivity extends AppCompatActivity {
         for(int i = 0;i<lijst.size();i++){
             data += lijst.get(i);
             data += "\n";
+            myRef.setValue(lijst.get(i));
         }
         if(data==""){
             data = "no data found.";
