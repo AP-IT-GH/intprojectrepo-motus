5205089e8bb0987c1faa6a1b5dc57084163e6692
Wannes <wanvinken@gmail.com>
2020-03-03 17:08:31 +0100

    Made Dummy Button Activity

---
+++ Android/app/src/main/java/com/example/motus/DummyButton.java
@@ -0,0 +1,14 @@
+package com.example.motus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DummyButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_button);
+    }
+}
