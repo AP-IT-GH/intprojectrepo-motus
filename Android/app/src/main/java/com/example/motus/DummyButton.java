b803ff3797e187de22d39c2d4476a8528a12d2c9
Wannes <wanvinken@gmail.com>
2020-03-10 15:00:32 +0100

    Finished Dummy Measure Buttons
    Added a Measuring Activity with a template button to start and cancel measuring.

--- Android/app/src/main/java/com/example/motus/DummyButton.java
+++ Android/app/src/main/java/com/example/motus/DummyButton.java
@@ -1,14 +1,40 @@
 package com.example.motus;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class DummyButton extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy_button);
+
+        final Button startMeasureButton = findViewById(R.id.start_measuring_button);
+
+        startMeasureButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d(LOG_TAG, "Pressed Button");
+                launchMeasuringActivity(view);
+            }
+        });
+    }
+
+    public void launchMeasuringActivity(View view){
+        Log.d(LOG_TAG, "Launching MeasuringActivity");
+
+        Intent intent = new Intent(this, MeasuringActivity.class);
+        startActivity(intent);
     }
 }
