911622151d8ceee0911c29167843be8a32ec7cc2
Maximilian Senten <maximilian.senten00@gmail.com>
2020-04-14 13:22:08 +0200

    werkend vm

--- Android/app/src/main/java/com/example/motus/LoginActivity.java
+++ Android/app/src/main/java/com/example/motus/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.motus;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -91,9 +93,10 @@ public class LoginActivity  extends NavigationMenu {
                 btnSignOut.setVisibility(View.INVISIBLE);
             }
         });
-
-        Intent intent = new Intent(this,ProfileInfo.class);
-        startActivity(intent);
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_login, null, false);
+        drawer.addView(contentView, 0);
     }
     private void signIn(){
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
