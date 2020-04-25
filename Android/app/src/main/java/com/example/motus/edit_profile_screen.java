568476c24ac949d1d0753a0e349ec50466e34b36
Wannes <wanvinken@gmail.com>
2020-04-25 15:40:46 +0200

    Connected profile screen to google
    Detached to edit profile screen activity, because you can't Change your google variables. And we are only going to try and connect through google.

--- Android/app/src/main/java/com/example/motus/edit_profile_screen.java
+++ Android/app/src/main/java/com/example/motus/edit_profile_screen.java
@@ -7,14 +7,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class edit_profile_screen extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
+    TextView Email;
+    TextView Name;
+    ImageView ProfilePicture;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference newRef = database.getReference("users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile_screen);
 
+        Email = findViewById(R.id.editable_email);
+        Name = findViewById(R.id.editable_username);
+        ProfilePicture = findViewById(R.id.editable_profile_picture);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        Email.setHint(user.getEmail());
+        Name.setHint(user.getDisplayName());
+        ProfilePicture.setImageURI(user.getPhotoUrl());
 
         final Button ConfirmChangeButton = findViewById(R.id.button_confirm_changed_userdata);
         final Button CancelChangeButton = findViewById(R.id.button_cancel_changed_userdata);
@@ -31,6 +54,7 @@ public class edit_profile_screen extends AppCompatActivity {
                 CancelChanges(v);
             }
         });
+
 
     }
     public void ConfirmChanges (View view){
