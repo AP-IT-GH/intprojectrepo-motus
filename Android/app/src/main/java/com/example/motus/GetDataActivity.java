698ec7e461d45d0c8ffcd987c27cf1217758a57b
Bart <s110266@ap.be>
2020-04-28 23:21:55 +0200

    rechanged

--- Android/app/src/main/java/com/example/motus/GetDataActivity.java
+++ Android/app/src/main/java/com/example/motus/GetDataActivity.java
@@ -75,6 +75,7 @@ public class GetDataActivity extends AppCompatActivity implements OnItemSelected
         currRef.orderByChild("uid").equalTo(UID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot){
+                dataSnapshot.getValue().toString();
                 ArrayList<HashMap<String,String>> data = (ArrayList<HashMap<String,String>>) dataSnapshot.getValue();
                 dataArray = data;
                 initialiseSpinner();
@@ -167,7 +168,7 @@ public class GetDataActivity extends AppCompatActivity implements OnItemSelected
             double y;
             for(int i = 0;i<dataArray.size();i++){
                 if(dataArray.get(i)!=null){
-                    String yValue = dataArray.get(i).get(dataPoint1).replace(' ','');
+                    String yValue = dataArray.get(i).get(dataPoint1).replace(" ","");
                     y = Double.parseDouble(yValue);
                     //nieuw datapunt aanmaken en toevoegen aan de serie datapunten
                     datapoints.add(y);
@@ -250,8 +251,8 @@ public class GetDataActivity extends AppCompatActivity implements OnItemSelected
         //we gaan voor elk punt dat we willen een nieuw datapunt aanmaken en toevoegen aan de serie
         for(int i = 0;i<dataArray.size();i++){
             if(dataArray.get(i)!=null){
-                String yValue = dataArray.get(i).get(dataPoint1).replace(' ','');
-                String xValue = dataArray.get(i).get(dataPoint2).replace(' ','');
+                String yValue = dataArray.get(i).get(dataPoint1).replace(" ","");
+                String xValue = dataArray.get(i).get(dataPoint2).replace(" ","");
                 y = Double.parseDouble(yValue);
                 x = Double.parseDouble(xValue);
                 //nieuw datapunt aanmaken en toevoegen aan de serie datapunten
